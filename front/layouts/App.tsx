ca3fdd5c5848be828bf489c080c0c515bebc9aff
JUNYEONG-K <godoly1211@naver.com>
2023-11-01 22:32:28 +0900

    feat. 회원가입, 로그인 페이지 기초 작성 & 코드 스플리팅

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom"; // Updated imports
+import loadable from '@loadable/component';
+
+// 코드 스플리팅 -> 필요할 때마다 불러오도록
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
     return (
-        <div>쫌만 기다려</div>
+        <Routes>
+            <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect */}
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+        </Routes>
     );
 }
 
