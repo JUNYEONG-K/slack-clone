d89f79fb70cad32d738019f5b179de75907af38e
JUNYEONG-K <godoly1211@naver.com>
2023-11-04 14:41:43 +0900

    feat. 회원가입 기본 양식

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -2,16 +2,37 @@ import React, {useCallback, useState} from "react";
 import { Form, Label, Input, Button, Header, Error, Success, LinkContainer } from "@pages/SignUp/styles";
 
 const SignUp = () => {
-    const [email] = useState('');
-    const [nickname] = useState('');
-    const [password] = useState('');
-    const [passwordCheck] = useState('');
+    const [email, setEmail] = useState('');
+    const [nickname, setNickname] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [mismatchError, setMismatchError] = useState(false);
 
-    const onChangeEmail = useCallback(() => {}, []);
-    const onChangeNickname = useCallback(() => {}, []);
-    const onChangePassword = useCallback(() => {}, []);
-    const onChangePasswordCheck = useCallback(() => {}, []);
-    const onSubmit = useCallback(() => {}, []);
+    const onChangeEmail = useCallback((e: { target: { value: React.SetStateAction<string>; }; }) => {
+        setEmail(e.target.value);
+    }, []);
+
+    const onChangeNickname = useCallback((e: { target: { value: React.SetStateAction<string>; }; }) => {
+        setNickname(e.target.value);
+    }, []);
+
+    const onChangePassword = useCallback((e: { target: { value: React.SetStateAction<string>; }; }) => {
+        setPassword(e.target.value);
+        setMismatchError(e.target.value !== passwordCheck);
+    }, [passwordCheck]);
+
+    const onChangePasswordCheck = useCallback((e: { target: { value: React.SetStateAction<string>; }; }) => {
+        setPasswordCheck(e.target.value);
+        setMismatchError(e.target.value !== password);
+    }, [password]);
+
+    const onSubmit = useCallback((e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        console.log(email, nickname, password, passwordCheck);
+        if (!mismatchError) {
+            console.log('서버로 회원가입하기');
+        }
+    }, [email, nickname, password, passwordCheck]);
 
     return (
         <div id="container">
@@ -46,8 +67,8 @@ const SignUp = () => {
                             onChange={onChangePasswordCheck}
                         />
                     </div>
-                    {/*{mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}*/}
-                    {/*{!nickname && <Error>닉네임을 입력해주세요.</Error>}*/}
+                    {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+                    {!nickname && <Error>닉네임을 입력해주세요.</Error>}
                     {/*{signUpError && <Error>{signUpError}</Error>}*/}
                     {/*{signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}*/}
                 </Label>
