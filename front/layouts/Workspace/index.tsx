63c0b7ab905bfc22be1677bbc185b1c089dacbe7
JUNYEONG-K <godoly1211@naver.com>
2023-11-13 12:58:27 +0900

    feat. 타입 지정

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -2,12 +2,13 @@ import React, {useCallback, useState} from "react";
 import useSWR from "swr";
 import fetcher from "@utils/fetcher";
 import axios from "axios";
-import {Navigate, Route, Routes} from "react-router-dom";
+import {Link, Navigate, Route, Routes} from "react-router-dom";
 import {
+    AddButton,
     Channels, Chats,
     Header, LogOutButton, MenuScroll,
     ProfileImg, ProfileModal,
-    RightMenu,
+    RightMenu, WorkspaceButton,
     WorkspaceName,
     Workspaces,
     WorkspaceWrapper
@@ -15,13 +16,14 @@ import {
 import gravatar from 'gravatar';
 import loadable from "@loadable/component";
 import Menu from "@components/Menu";
+import {IUser} from "@typings/db";
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DM'));
 
 const Workspace = () => {
     const [showUserMenu, setShowUserMenu] = useState(false);
-    const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
+    const { data: userData, error, mutate } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher);
 
     const onLogout = useCallback(() => {
         axios.post('http://localhost:3095/api/users/logout', null, {
@@ -32,11 +34,15 @@ const Workspace = () => {
             })
     }, []);
 
-    const onClickUserMenu = useCallback(() => {
+    const  onClickUserMenu = useCallback(() => {
         setShowUserMenu((prev) => !prev);
     }, []);
 
-    if (!data) {
+    const onClickCreateWorkspace = useCallback(() => {
+
+    }, []);
+
+    if (!userData) {
         return <Navigate to="/login" />;
     }
 
@@ -46,18 +52,18 @@ const Workspace = () => {
                 <RightMenu>
                     <span onClick={onClickUserMenu}>
                         <ProfileImg
-                            src={gravatar.url(data.email, { s: '28px', d: 'retro' })}
-                            alt={data.nickname}
+                            src={gravatar.url(userData.email, { s: '28px', d: 'retro' })}
+                            alt={userData.nickname}
                         />
                         { showUserMenu && (
                             <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseMenu={onClickUserMenu}>
                                 <ProfileModal>
                                      <img
-                                        src={gravatar.url(data.email, { s: '28px', d: 'retro' })}
-                                        alt={data.nickname}
+                                        src={gravatar.url(userData.email, { s: '28px', d: 'retro' })}
+                                        alt={userData.nickname}
                                     />
                                     <div>
-                                        <span id="profile-name">{data.nickname}</span>
+                                        <span id="profile-name">{userData.nickname}</span>
                                         <span id="profile-active">Active</span>
                                     </div>
                                 </ProfileModal>
@@ -70,7 +76,16 @@ const Workspace = () => {
             <button onClick={onLogout}>로그아웃</button>
             <WorkspaceWrapper>
                 <Workspaces>
-                    test
+                    {userData.Workspaces.map((workspace) => {
+                        return (
+                            <Link key={workspace.id} to={`workspace/${123}/channel/일반`}>
+                                <WorkspaceButton>
+                                    {workspace.name.slice(0, 1).toUpperCase()}
+                                </WorkspaceButton>
+                            </Link>
+                        )
+                    })}
+                    <AddButton onClick={onClickCreateWorkspace}>+</AddButton>
                 </Workspaces>
                 <Channels>
                     <WorkspaceName>
